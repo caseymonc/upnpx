15e1943e0bb107edb735d747e4e24e3420c06271
jonguan@gmail.com <jonguan@gmail.com@e6ec9849-c368-528d-2c89-72666b4bbcf2>
2013-04-30 21:22:26 +0000

    multi-threaded support and memory leak cleanup
    git-svn-id: http://upnpx.googlecode.com/svn@323 e6ec9849-c368-528d-2c89-72666b4bbcf2

---
+++ branches/upnpx-1.2.5/upnp-ios/upnp-ios/upnp_ios.h
@@ -0,0 +1,13 @@
+//
+//  upnp_ios.h
+//  upnp-ios
+//
+//  Created by Jonathan.Guan on 7/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface upnp_ios : NSObject
+
+@end
---
+++ branches/upnpx-1.2.5/upnp-ios/upnp-ios/upnp_ios.m
@@ -0,0 +1,13 @@
+//
+//  upnp_ios.m
+//  upnp-ios
+//
+//  Created by Jonathan.Guan on 7/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "upnp_ios.h"
+
+@implementation upnp_ios
+
+@end
